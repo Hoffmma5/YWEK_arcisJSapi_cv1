347d13b3d7bc4080f7ceb0709c8886466be89ff3
Hoffmma5 <75030565+Hoffmma5@users.noreply.github.com>
2024-04-13 21:46:32 +0200

    moduly

---
+++ script.js
@@ -0,0 +1,14 @@
+// import {foo, foo2} from './testModul.js'
+
+// foo()
+// foo2()
+
+// import {foo as myFoo} from './testModul.js'
+
+// myFoo();
+
+import foo from './testModul.js'
+import {foo2} from './testModul.js'
+
+foo()
+foo2()
