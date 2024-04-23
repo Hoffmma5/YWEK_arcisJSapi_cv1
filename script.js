f522694538a82511c375d8cfece88cabb0f9708d
Hoffmma5 <75030565+Hoffmma5@users.noreply.github.com>
2024-04-23 19:11:57 +0200

    Finální aplikace ze druhého cvičení

--- script.js
+++ script.js
@@ -1,174 +1,124 @@
-require([
-  "esri/WebMap",
-  "esri/views/MapView",
-  "esri/widgets/Sketch",
-  "esri/layers/GraphicsLayer",
-  "esri/widgets/Search",
-  "esri/widgets/ScaleBar",
-  "esri/widgets/LayerList",
-  "esri/widgets/BasemapGallery",
-  "esri/widgets/TimeSlider"
-], (
-  WebMap,
-  MapView,
-  Sketch,
-  GraphicsLayer,
-  Search,
-  ScaleBar,
-  LayerList,
-  BasemapGallery,
-  TimeSlider
-) => {
-  // INICIALIZACE
-  const graphicsLayer = new GraphicsLayer();
-  let programLayerView;
-  let actionBarExpanded = false;
+require(["esri/Map",
+    "esri/WebMap",
+    "esri/views/MapView",
+    "esri/layers/FeatureLayer",
+    "esri/widgets/BasemapGallery",
+    "esri/widgets/ScaleBar",
+    "esri/widgets/LayerList",
+    "esri/widgets/Expand"],
+    (Map, WebMap, MapView, FeatureLayer, BasemapGallery, ScaleBar, LayerList, Expand) => {
 
-  const programFilter = document.getElementById("program-filter");
-  const clearBtn = document.getElementById("clearFilter");
+        let programLayerView;
 
-  const webmap = new WebMap({
-    portalItem: {
-      id: "095b6944f4764c4a8ebf5d4483d7ee22",
-    },
-  });
+        const programNodes = document.querySelectorAll(`.program-item`);
+        const programElement = document.getElementById("program-filter");
+        const clearButton = document.getElementById("clearFilter");
 
-  const mapView = new MapView({
-    container: "viewDiv",
-    map: webmap,
+        const webmap = new WebMap({
+            portalItem: { // autocasts as new PortalItem()
+                id: "095b6944f4764c4a8ebf5d4483d7ee22"
+            }
+        });
 
-    padding: {
-      left: 49,
-    },
-  });
+        const view = new MapView({
+            container: "viewDiv",
+            map: webmap,
+            padding: {
+                left: 50
+            }
+        });
 
-  // OVLADACE UDALOSTI
-  programFilter.addEventListener("click", filterByProgram);
-  clearBtn.addEventListener("click", () => {
-    programLayerView.filter = null;
-  });
+        const basemapGallery = new BasemapGallery({
+            view: view,
+            container: "basemapGalleryContainer"
+        });
 
-  // FILTRACE
+        const scaleBar = new ScaleBar({
+            view: view,
+            style: "ruler",
+            unit: "dual"
+        });
+        // Add widget to the bottom left corner of the view
+        view.ui.add(scaleBar, {
+            position: "bottom-left"
+        });
 
-  /**
-   * Nastaveni atributoveho filtru na layerView
-   * @param {*} event
-   */
-  function filterByProgram(event) {
-    const selectedProgram = event.target.getAttribute("data-program");
-    if (selectedProgram !== "BaN") {
-      programLayerView.filter = {
-        where: "typ_studia = '" + selectedProgram + "'",
-      };
-    }
-  }
+        let layerList = new LayerList({
+            view: view,
+            container: "layerListContainer",
+            dragEnabled: true
+        });
 
-  // PRACE S MAPVIEW
-  mapView.when(() => {
-    const layer = webmap.allLayers.find(function (layer) {
-      return layer.title === "studentiGaK";
+
+        // click event handler for seasons choices
+        programElement.addEventListener("click", filterByProgram);
+        clearButton.addEventListener("click", (event) => {
+            programLayerView.filter = null;
+        });
+
+        // User clicked on Winter, Spring, Summer or Fall
+        // set an attribute filter on flood warnings layer view
+        // to display the warnings issued in that season
+        function filterByProgram(event) {
+            const selectedProgram = event.target.getAttribute("data-program");
+            if (selectedProgram !== 'BaN') {
+                programLayerView.filter = {
+                    where: "typ_studia = '" + selectedProgram + "'"
+                };
+            }
+        }
+
+        view.when(() => {
+
+            const foundLayer = webmap.allLayers.find(function (layer) {
+                return layer.title === "studentiGaK";
+            });
+
+            view.whenLayerView(foundLayer).then((layerView) => {
+                // flash flood warnings layer loaded
+                // get a reference to the flood warnings layerview
+                programLayerView = layerView;
+            });
+
+            const { title } = webmap.portalItem;
+            document.querySelector("#header-title").textContent = title;
+
+            let activeWidget;
+
+            const handleActionBarClick = ({ target }) => {
+                console.log(activeWidget);
+                if (target.tagName !== "CALCITE-ACTION") {
+                    return;
+                }
+
+                if (activeWidget) {
+                    document.querySelector(`[data-action-id=${activeWidget}]`).active = false;
+                    document.querySelector(`[data-panel-id=${activeWidget}]`).hidden = true;
+                }
+
+                const nextWidget = target.dataset.actionId;
+                console.log(nextWidget);
+                if (nextWidget !== activeWidget) {
+                    document.querySelector(`[data-action-id=${nextWidget}]`).active = true;
+                    document.querySelector(`[data-panel-id=${nextWidget}]`).hidden = false;
+                    activeWidget = nextWidget;
+                } else {
+                    activeWidget = null;
+                }
+            };
+
+            document.querySelector("calcite-action-bar").addEventListener("click", handleActionBarClick);
+
+        });
+
+        /********************
+         * Add feature layer
+         ********************/
+
+        // Carbon storage of trees in Warren Wilson College.
+        // const featureLayer = new FeatureLayer({
+        //     url: "https://services2.arcgis.com/LlQJoYHf9cwrM7NJ/arcgis/rest/services/studentiGaK/FeatureServer"
+        // });
+
+        // webmap.add(featureLayer);
     });
-
-    mapView.whenLayerView(layer).then((layerView) => {
-      programLayerView = layerView; // ulozeni layerView do promenne
-      timeSlider.fullTimeExtent = layer.timeInfo.fullTimeExtent.expandTo("years");
-      timeSlider.stops = {
-        interval: layer.timeInfo.interval
-      };
-    });
-
-    const sketch = new Sketch({
-      layer: graphicsLayer,
-      view: mapView,
-      creationMode: "single",
-      container: sketchContainer,
-    });
-
-    const timeSlider = new TimeSlider({
-      container: "timeSliderContainer",
-      mode: "time-window",
-      view: mapView
-    });
-
-    // search widget
-    const searchWidget = new Search({
-      view: mapView,
-    });
-
-    // pridani searchWidgetu do mapy
-    mapView.ui.add(searchWidget, {
-      position: "top-left",
-      index: 0, // udava poradi widgetu nad sebou v rohu v mape
-    });
-
-    // scaleBar widget
-    const scaleBar = new ScaleBar({
-      view: mapView,
-      style: "line",
-      unit: "dual",
-    });
-
-    // pridani meritka do mapy
-    mapView.ui.add(scaleBar, {
-      position: "bottom-left",
-    });
-
-    // layerList widget
-    const layerList = new LayerList({
-      view: mapView,
-      container: layerListContainer,
-      dragEnabled: true,
-    });
-
-    // BasemapGallery widget
-    const basemapGallery = new BasemapGallery({
-      view: mapView,
-      container: basemapGalleryContainer,
-    });
-
-    let activeWidget;
-
-    /**
-     * Funkce ovladajici kliknuti na action bar
-     *
-     * @param {Object} target - target kliknuti.
-     * @return {void} nic nevraci
-     */
-    const handleActionBarClick = ({ target }) => {
-      if (target.tagName !== "CALCITE-ACTION") {
-        return;
-      }
-
-      if (activeWidget) {
-        document.querySelector(
-          `[data-action-id=${activeWidget}]`
-        ).active = false;
-        document.querySelector(`[data-panel-id=${activeWidget}]`).hidden = true;
-      }
-
-      const nextWidget = target.dataset.actionId;
-      if (nextWidget !== activeWidget) {
-        document.querySelector(`[data-action-id=${nextWidget}]`).active = true;
-        document.querySelector(`[data-panel-id=${nextWidget}]`).hidden = false;
-        activeWidget = nextWidget;
-      } else {
-        activeWidget = null;
-      }
-    };
-
-    document
-      .querySelector("calcite-action-bar")
-      .addEventListener("click", handleActionBarClick);
-
-    document.addEventListener("calciteActionBarToggle", (event) => {
-      actionBarExpanded = !actionBarExpanded;
-      mapView.padding = {
-        left: actionBarExpanded ? 135 : 49,
-      };
-    });
-
-    document.querySelector("calcite-shell").hidden = false;
-  });
-
-  webmap.add(graphicsLayer);
-});
